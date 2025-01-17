37e907b6bc8c556213ee51aed2d052d3c10672a1
danefriser3 <parisi_daniele@hotmail.it>
2025-01-18 00:13:11 +0100

    Add CORS support and enhance expense and bank account endpoints

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
+const cors = require("cors");
 import { createClient } from "@vercel/postgres";
 import { Request, Response } from "express";
 
@@ -14,43 +15,125 @@ const client = createClient();
 client.connect();
 
 app.use(express.static("public"));
+app.use(cors());
+app.use(express.json()); // Parse JSON bodies
 
-app.get("/", function (req: Request, res: Response) {
-  res.send("Express on Vercel");
+// Expense model
+export interface Expense {
+  id: string;
+  name: string;
+  amount: number;
+  category: string;
+  date: string;
+  bankAccount: string;
+  details: string;
+}
+
+export interface BankAccount {
+  id: string;
+  name: string;
+  base_value: number;
+}
+
+app.get("/bank_accounts", async (req: Request, res: Response) => {
+  try {
+    const { rows: bank_accounts } =
+      await client.sql`SELECT * FROM bank_accounts`;
+    res.status(200).json(
+      bank_accounts.map((bank_account) => ({
+        ...bank_account,
+        base_value: parseFloat(bank_account.base_value),
+      }))
+    );
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error fetching expenses");
+  }
 });
 
-app.post(
-  "/uploadSuccessful",
+app.put(
+  "/bank_accounts/:id",
   urlencodedParser,
   async (req: Request, res: Response) => {
+    const { name, base_value }: Partial<BankAccount> = req.body;
+    console.log(name, base_value, req.params.id);
     try {
-      // await sql`INSERT INTO Users (Id, Name, Email) VALUES (${req.body.user_id}, ${req.body.name}, ${req.body.email});`;
-      res.status(200).send("<h1>User added successfully</h1>");
+      await client.sql`UPDATE bank_accounts SET name = ${name}, base_value = ${base_value} WHERE id = ${req.params.id}`;
+      res.status(200).send("Bank Account updated successfully");
     } catch (error) {
       console.error(error);
-      res.status(500).send("Error adding user");
+      res.status(500).send("Error updating bank account");
     }
   }
 );
 
-app.get("/allUsers", async (req: Request, res: Response) => {
+app.get("/expenses/:bankAccount", async (req: Request, res: Response) => {
   try {
-    const { rows: users } = await client.sql`SELECT * from users`;
-    res.status(200).send({ users });
+    const { rows: expenses } =
+      await client.sql`SELECT * FROM expenses WHERE bank_account = ${req.params.bankAccount}`;
+    res.status(200).json(
+      expenses.map((expense) => ({
+        ...expense,
+        amount: parseFloat(expense.amount),
+      }))
+    );
   } catch (error) {
     console.error(error);
-    res.status(500).send("Error sequencing user");
+    res.status(500).send("Error fetching expenses");
   }
 });
 
-app.get("/user/:id", async (req: Request, res: Response) => {
+app.get("/expenses/:id", async (req: Request, res: Response) => {
   try {
-    const { rows: user } =
-      await client.sql`SELECT * from users where id = ${req.params.id}`;
-    res.status(200).send({ user });
+    const { rows: expense } =
+      await client.sql`SELECT * FROM expenses WHERE id = ${req.params.id}`;
+    if (expense.length === 0) {
+      res.status(404).send("Expense not found");
+    } else {
+      res.status(200).json(expense[0]);
+    }
   } catch (error) {
     console.error(error);
-    res.status(500).send("Error sequencing user");
+    res.status(500).send("Error fetching expense");
+  }
+});
+
+app.post("/expenses", urlencodedParser, async (req: Request, res: Response) => {
+  const { name, amount, category, date, bankAccount, details }: Expense =
+    req.body;
+  try {
+    await client.sql`INSERT INTO expenses (name, amount, category, date, bank_account, details) VALUES (${name}, ${amount}, ${category}, ${date}, ${bankAccount}, ${
+      details ?? ""
+    })`;
+    res.status(201).send("Expense added successfully");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error adding expense");
+  }
+});
+
+app.put(
+  "/expenses/:id",
+  urlencodedParser,
+  async (req: Request, res: Response) => {
+    const { name, amount, category, date }: Partial<Expense> = req.body;
+    try {
+      await client.sql`UPDATE expenses SET name = ${name}, amount = ${amount}, category = ${category}, date = ${date} WHERE id = ${req.params.id}`;
+      res.status(200).send("Expense updated successfully");
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Error updating expense");
+    }
+  }
+);
+
+app.delete("/expenses/:id", async (req: Request, res: Response) => {
+  try {
+    await client.sql`DELETE FROM expenses WHERE id = ${req.params.id}`;
+    res.status(200).send("Expense deleted successfully");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error deleting expense");
   }
 });
 
